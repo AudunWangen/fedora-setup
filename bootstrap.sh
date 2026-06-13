7ce21916336c284a468c41f139c465fbfcd79ad0
Audun Wangen <audun.wangen@gmail.com>
2026-06-13 21:57:06 +0200

    Fixed privilege bug in bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,7 @@ REPO_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd ${REPO_ROOT}
 
 echo "Installing prerequisites"
-dnf install -y ansible
+sudo dnf install -y ansible
 
 echo "Running the basic Ansible playbook"
 ansible-playbook -i ansible/inventory ansible/init.yml -e "dotfiles_repo_root=${REPO_ROOT}" --ask-become-pass
